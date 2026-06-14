b36d702e8428e0de7174ce6d550cb4f0ba1277bc
MoltenNaut <moltencrysto558810@gmail.com>
2026-06-14 16:22:24 -0700

    TIME TO MEGRE

--- water_bullet.gd
+++ water_bullet.gd
@@ -9,3 +9,11 @@ func _physics_process(delta: float) -> void:
 
 func _on_timer_timeout() -> void:
 	queue_free()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if !body.is_in_group("Player"):
+		if body.is_in_group("Enemy"):
+			body.Stun()
+		else:
+			queue_free()
