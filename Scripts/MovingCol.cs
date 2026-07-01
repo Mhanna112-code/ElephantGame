9ccbe1d4474fa6b11875878f8c2d75e8cc03811f
Mhanna112-code <60158039+Mhanna112-code@users.noreply.github.com>
2026-07-01 16:50:47 -0500

    added moving body

---
+++ Scripts/MovingCol.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class MovingColumn : MonoBehaviour
+{
+    [SerializeField] private float moveDistance = 3f;
+    [SerializeField] private float speed = 2f;
+
+    private Vector3 startPos;
+
+    private void Start()
+    {
+        startPos = transform.position;
+    }
+
+    private void Update()
+    {
+        float offset = Mathf.Sin(Time.time * speed) * moveDistance;
+
+        transform.position = startPos + Vector3.up * offset;
+    }
+}
---
+++ Scripts/Travel.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Travel : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private Transform cameraTransform;
+    private Vector3 cameraStartOffset;
+    [Header("Jump")]
+    [SerializeField] private float jumpForce = 7f;
+
+    [SerializeField] private float fallLimit = -10f;
+
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position; // save spawn point
+        if (cameraTransform != null)
+        {
+            cameraStartOffset = cameraTransform.position - transform.position;
+        }
+
+    }
+
+    void Update()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        transform.position += transform.forward * moveSpeed * Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
+        CheckFall();
+    }
+
+    void CheckFall()
+    {
+        if (transform.position.y < fallLimit) {
+            Respawn();
+        }
+    }
+
+    void Respawn()
+    {
+       /* transform.position = startPosition;
+        transform.rotation = Quaternion.identity;
+        // optional: reset velocity if using Rigidbody
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.linearVelocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+        ResetCamera();*/
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+    }
+
+    void ResetCamera()
+{
+    if (cameraTransform != null)
+    {
+        cameraTransform.position = startPosition + cameraStartOffset;
+        cameraTransform.rotation = Quaternion.identity;
+    }
+}
+}
